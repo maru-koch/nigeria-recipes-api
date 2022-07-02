f932d87b7881749bda9a6a7385faac740a2ae4a4
maru-koch <nwokochafranklyn@gmail.com>
2022-07-02 19:19:22 +0100

    feature

--- nigeria_recipes/api/models.py
+++ nigeria_recipes/api/models.py
@@ -1,6 +1,6 @@
 from uuid import uuid5
 from django.db import models
-from django.forms import UUIDField, uuid5
+from django.forms import UUIDField
 
 # Create your models here.
 class Ingredients(models.Model):
@@ -15,7 +15,7 @@ class Category(models.Model):
         return self.name
 
 class Meal(models.Model):
-    id = models.UUIDField(primary_key=True, editable=False, default=uuid5)
+    id = models.UUIDField(primary_key=True, editable=False)
     image = models.ImageField(upload_to="images")
     title = models.CharField(max_length=100)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
@@ -25,14 +25,14 @@ class Meal(models.Model):
         return self.title
 
 class Recipes(models.Model):
-    meal = models.ForeignKey(Meal, on_delete=models.SET_NULL)
+    meal = models.ForeignKey(Meal, on_delete=models.CASCADE)
     ingredient=models.ManyToManyField(Ingredients)
     quantity=models.IntegerField()
     def __str__(self):
         return f"{self.meal} recipes"
 
 class Preparation(models.Model):
-    meal = models.ForeignKey(Meal)
+    meal = models.ForeignKey(Meal, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
     duration = models.CharField(max_length=200)
